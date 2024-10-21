e294509ed8569231b1f0d2422a5f9eca8e1e2899
minjunseok <tjralswnssla@naver.com>
2024-10-21 16:03:23 +0900

    유데미 리액트 강의 2024.10.21

--- onebite-react/section05/src/App.jsx
+++ onebite-react/section05/src/App.jsx
@@ -1,13 +1,21 @@
 
 import './App.css'
+import Header from './component/Header.jsx';
+
+
+
+
+
+
 
 function App() {
 
   return (
     <>
+    <Header />
     <h1>안녕하세요 개발자 석민준입니다.</h1>
     </>
   )
 }
 
-export default App
+export default App;
---
+++ onebite-react/section05/src/Components/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+
+  return (
+
+    <header>
+      <h1>header</h1>
+    </header>
+
+  );
+
+};
+
+export default Header;
