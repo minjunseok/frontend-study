a917ef6c7a5f70e89f1eca95335ea758921ec099
minjunseok <tjralswnssla@naver.com>
2024-11-04 16:35:23 +0900

    유데미 리액트 강의 2024.11.4

--- onebite-react/section05/src/App.jsx
+++ onebite-react/section05/src/App.jsx
@@ -1,37 +1,17 @@
 
 import './App.css'
-
-import Main from './components/Main';
-import Button from './components/Button';
-import Header from './components/Header';
-
-
-
-
-
-
-
+import {useState} from 'react';
+import Bulb from './components/Bulb';
+import Counter from './components/Counter';
 
 
 function App() {
 
-  const buttonProps = {
-
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3,
-  };
 
   return (
     <>
-
-      <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"} >
-       <Header/>
-        </Button>
+      <Bulb />
+    <Counter />
 
     </>
   )
