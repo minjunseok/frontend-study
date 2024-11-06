d5f8537ac3d400f9f8a94cbcaca1f9e2b5f0f045
minjunseok <tjralswnssla@naver.com>
2024-11-06 23:55:01 +0900

    유데미 리액트 강의 2024.11.6

---
+++ onebite-react/section05/src/Components/Register.jsx
@@ -0,0 +1,36 @@
+
+import { useState } from 'react'; 
+
+// 간단한 회원가입 폼
+// 1. 이름
+// 2. 생년월일
+// 3. 국적
+// 4. 자기소개
+
+
+const Register=()=>{
+
+
+  const [name , setName] = useState("이름을 입력해주세요");
+
+  const onChangeName = (e)=>{
+
+    setName(e.target.value);
+
+
+  }
+
+  return (
+  
+  <div>
+    <input
+     value={name}
+     onChange={onChangeName}
+     placeholder={"이름"}
+      />
+      {name}
+  </div>
+  );
+};
+
+export default Register;
