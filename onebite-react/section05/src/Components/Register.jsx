2e0792483f138a37ebcfb50546dac6518bc587ba
minjunseok <tjralswnssla@naver.com>
2024-11-07 16:47:10 +0900

    유데미 리액트 강의 2024.11.7

--- onebite-react/section05/src/Components/Register.jsx
+++ onebite-react/section05/src/Components/Register.jsx
@@ -1,5 +1,4 @@
-
-import { useState } from 'react'; 
+import { useState } from "react";
 
 // 간단한 회원가입 폼
 // 1. 이름
@@ -7,29 +6,71 @@ import { useState } from 'react';
 // 3. 국적
 // 4. 자기소개
 
+const Register = () => {
+  const [name, setName] = useState("이름을 입력해주세요");
+  const [birth, setBirth] = useState("");
+  const [country, setCountry] = useState("");
+  const [bio, setBio] = useState("");
 
-const Register=()=>{
-
-
-  const [name , setName] = useState("이름을 입력해주세요");
-
-  const onChangeName = (e)=>{
-
+  const onChangeName = (e) => {
     setName(e.target.value);
+  };
 
+  const onChangeBirth = (e) => {
+    setBirth(e.target.value);
+  };
 
-  }
+  const onChangeCountry = (e) => {
+
+    setCountry(e.target.value);
+
+  };
+
+  const onChangeBio = (e) => {
+
+    setBio(e.target.value);
+
+  };
 
   return (
-  
-  <div>
-    <input
-     value={name}
-     onChange={onChangeName}
-     placeholder={"이름"}
-      />
-      {name}
-  </div>
+    <div>
+      <div>
+        <input
+         value={name}
+         onChange={onChangeName}
+         placeholder={"이름"}
+         />
+
+      </div>
+
+      <div>
+        <input
+         value={birth}
+         onChange={onChangeBirth}
+         type="date"
+        />
+   
+      </div>
+        <select
+         value={country}
+         onChange={onChangeCountry}
+         >
+          <option value=""></option>
+          <option value="kr">한국</option>
+          <option value="us">미국</option>
+          <option value="uk">영국</option>
+        </select>
+        {country}
+
+      <div>
+        <textarea 
+         value={bio}
+         onChange={onChangeBio}
+         />
+         {bio}
+
+      </div>
+    </div>
   );
 };
 
