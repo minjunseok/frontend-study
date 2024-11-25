1387571ad906d6f67bd6dec87cbd96923a318477
minjunseok <tjralswnssla@naver.com>
2024-11-25 19:02:40 +0900

    오둥이 TodoList UI 완성

--- onebite-react/section08/src/App.jsx
+++ onebite-react/section08/src/App.jsx
@@ -1,13 +1,22 @@
 
 import './App.css'
+import Editor from './components/Editor'
+import Header from './components/Header'
+import List from './components/List'
 
 function App() {
   
   return (
-    <>
-      TodoList
-    </>
-  )
-}
+
+    
+   <div className="App">
+
+      <Header />
+      <Editor />
+      <List />
+
+    </div>
+  );
+};
 
 export default App
