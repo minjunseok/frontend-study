1600f5706a01d69c2f2c81187d9399c9e77356bc
minjunseok <tjralswnssla@naver.com>
2024-11-25 22:53:02 +0900

    TodoList - 기능 구현 준비

--- onebite-react/section08/src/App.jsx
+++ onebite-react/section08/src/App.jsx
@@ -1,11 +1,40 @@
 
 import './App.css'
+import { useState } from "react";
 import Editor from './components/Editor'
 import Header from './components/Header'
 import List from './components/List'
+import React from 'react'
+
+
+
+const mockData= [
+  {
+    id:0,
+    isDone : false,
+    content : "React 공부하기",
+    date : new Date().getTime(),
+  },
+  {
+    id:1,
+    isDone : false,
+    content : "헬스장 가기",
+    date : new Date().getTime(),
+  },
+  {
+    id:2,
+    isDone : false,
+    content : "집안 정리하기",
+    date : new Date().getTime(),
+  }
+];
 
 function App() {
   
+
+
+  const [todos, setTodos] = useState(mockData);
+
   return (
 
     
