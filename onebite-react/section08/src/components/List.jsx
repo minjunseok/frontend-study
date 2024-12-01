d8a33d4b7b4f0a80679ae7ffabedcb71a2b8fa84
minjunseok <tjralswnssla@naver.com>
2024-12-02 01:47:58 +0900

    TodoList - List 구현

--- onebite-react/section08/src/components/List.jsx
+++ onebite-react/section08/src/components/List.jsx
@@ -1,23 +1,50 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import {useState} from 'react';
 
 const List = ({todos}) => {
 
+  const [search, setSearch] = useState("");
+  
+  const onChangeSearch = (e) => {
 
-  return (
+    setSearch(e.target.value);
 
-    <div className="List">
-      <h4>오둥이 Todo List 🌱</h4>
-      <input placeholder="검색어를 입력하세요!" />
-      <div className="todos_wrapper">
-    {todos.map((todo) => {
+  };
 
-      return <div>todo</div>;
+  const getFilteredData = () => {
 
-    })}
-      
+    if(search === "") {
+
+      return todos;
+
+    }
+    return todos.filter((todo)=>
+      todo.content.toLowerCase().includes(search.toLowerCase())
+    );
+
+  };
+
+  const filteredTodos = getFilteredData();
+
+
+      return (
+
+        <div className="List">
+          <h4>오둥이 Todo List 🌱</h4>
+            <input value={search}
+            onChange={onChangeSearch}
+            placeholder="검색어를 입력하세요!"
+          />
+          <div className="todos_wrapper">
+            {filteredTodos.map((todo) => {
+
+              return <TodoItem key={todo.id} {...todo} />;
+
+        })}
+          
       </div>
-      </div>
+    </div>
 
   );
 };
