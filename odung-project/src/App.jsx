39071c9e6580e14b6ff508f9c8eb5dc4ef984ac7
minjunseok <tjralswnssla@naver.com>
2025-01-04 21:27:52 +0900

    오둥이 가위바위보 모바일 최적화 보류

--- odung-project/src/App.jsx
+++ odung-project/src/App.jsx
@@ -153,6 +153,15 @@ const randomChoice =()=>{
 };
 
 
+
+function setScreenSize() {
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+}
+useEffect(() => {
+  setScreenSize();
+});
+
   return (
 
     <div>
