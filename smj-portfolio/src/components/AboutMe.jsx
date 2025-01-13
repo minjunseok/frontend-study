cdf985d93f1245428b0c9573dbe96a3c30bd0a90
minjunseok <tjralswnssla@naver.com>
2025-01-13 16:34:00 +0900

    포폴 완성

--- smj-portfolio/src/components/AboutMe.jsx
+++ smj-portfolio/src/components/AboutMe.jsx
@@ -1,5 +1,12 @@
 import '../css/AboutMe.css';
 import AboutItem from './AboutItem';
+import myphoto from '../assets/Myphoto.jpeg';
+import icon1 from '../assets/user-Icon.png';
+import icon2 from '../assets/mail-Icon.png';
+import icon3 from '../assets/date-Icon.png';
+import icon4 from '../assets/phone-Icon.png';
+import icon5 from '../assets/edu-Icon.png';
+import icon6 from '../assets/location-Icon.png';
 const AboutMe = () => {
   return (
 
@@ -10,15 +17,15 @@ const AboutMe = () => {
       </div>
         <div className='content'>
           <div className='myPhoto'>
-            <img src='src/assets/MyPhoto.jpeg'></img>
+            <img src={myphoto}></img>
           </div>
           <div className='items-grid'>
-            <AboutItem icon='src/assets/user-Icon.png' label='이름' value='석민준' />
-            <AboutItem icon='src/assets/mail-Icon.png' label='E-mail' value='tjralswnssla@naver.com'/> 
-            <AboutItem icon='src/assets/date-Icon.png' label='생년월일' value='95.05.26'/>
-            <AboutItem icon='src/assets/phone-Icon.png' label='연락처' value='010-3835-8722'/>
-            <AboutItem icon='src/assets/edu-Icon.png' label='학력' value='동서울대학교 디지털전자과'/>
-            <AboutItem icon='src/assets/location-Icon.png' label='지역' value='성남시 수정구'/>
+            <AboutItem icon={icon1} label='이름' value='석민준' />
+            <AboutItem icon={icon2} label='E-mail' value='tjralswnssla@naver.com'/> 
+            <AboutItem icon={icon3} label='생년월일' value='95.05.26'/>
+            <AboutItem icon={icon4} label='연락처' value='010-3835-8722'/>
+            <AboutItem icon={icon5} label='학력' value='동서울대학교 디지털전자과'/>
+            <AboutItem icon={icon6} label='지역' value='성남시 수정구'/>
           </div>
         </div>
      
