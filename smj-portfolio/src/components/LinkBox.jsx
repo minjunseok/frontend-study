0c46f078663743cf55d35bfa943d4c47c920c0cc
minjunseok <tjralswnssla@naver.com>
2025-01-17 22:43:31 +0900

    컨펀사항 수정중

--- smj-portfolio/src/components/LinkBox.jsx
+++ smj-portfolio/src/components/LinkBox.jsx
@@ -3,7 +3,7 @@ import '../css/LinkBox.css';
 const LinkBox = ({ img, content, link }) => {
 
   const handleClick = () => {
-    window.location.href = link; // 외부 링크로 이동
+    window.open(link); // 외부 링크로 이동
   };
 
   return (
